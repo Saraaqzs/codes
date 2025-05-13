dbd268e21451cadc648302f9b075966d010e5894
vdellabalda <vinc.dellabalda@gmail.com>
2025-05-13 10:47:43 +0200

    change setup

--- setup_oncomx.sh
+++ setup_oncomx.sh
@@ -6,19 +6,18 @@ cd ../
 git clone https://github.com/vdellabalda/pg2sqlite.git
 cd pg2sqlite/initial-sync/py
 pip install -r requirements.txt
+cp *.py /scratch/codes/data/sft_data_collections/oncomx
 cd ../../../
 
 git clone https://github.com/ckosten/sciencebenchmark_dataset.git
 
 cd codes/data/sft_data_collections
-rm -r oncomx
-mkdir oncomx
 cd oncomx
 
 #python ../../../../pg2sqlite/initial-sync/py/convert-schema.py "postgresql://uzh_user:bigdata%40uzh@160.85.252.195:5433/oncomx_v1_0_25?options=-csearch_path%3oncomx_v1_0_25" oncomx_v1_0_25_small.db
 #python ../../../../pg2sqlite/initial-sync/py/convert-schema.py "postgresql://uzh_user:bigdata%40uzh@160.85.252.195:5433/oncomx_v1_0_25?options=-csearch_path%3Doncomx_v1_0_25" oncomx_v1_0_25_small.db
-python ../../../../pg2sqlite/initial-sync/py/export_to_csv.py  "postgresql://uzh_user:bigdata%40uzh@160.85.252.195:5433/oncomx_v1_0_25?options=-csearch_path%3Doncomx_v1_0_25" 'oncomx_v1_0_25' oncomx_v1_0_25_small_dir
-python ../../../../pg2sqlite/initial-sync/py/import_csv_to_sqlite.py oncomx_v1_0_25_small_dir oncomx_v1_0_25_small.sqlite oncomx_v1_0_25_small.sql
+python export_to_csv.py  "postgresql://uzh_user:bigdata%40uzh@160.85.252.195:5433/oncomx_v1_0_25?options=-csearch_path%3Doncomx_v1_0_25" 'oncomx_v1_0_25' oncomx_v1_0_25_small_dir
+python import_csv_to_sqlite.py oncomx_v1_0_25_small_dir oncomx_v1_0_25_small.sqlite oncomx_v1_0_25_small.sql
 mkdir oncomx_v1_0_25_small
 mv oncomx_v1_0_25_small.sqlite ./oncomx_v1_0_25_small/oncomx_v1_0_25_small.sqlite
 
