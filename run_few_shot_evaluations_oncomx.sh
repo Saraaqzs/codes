5301d44c884f990cb8f3873647ada45e66373be5
Sara A. Safari <48481380+Saraaqzs@users.noreply.github.com>
2025-05-14 16:46:45 +0200

    adding line breaks

--- run_few_shot_evaluations_oncomx.sh
+++ run_few_shot_evaluations_oncomx.sh
@@ -2,15 +2,15 @@ set -e
 
 # --------------- Few-shot CodeS-1B --------------- #
 # Oncomx's dev (1-shot)
-CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py\ 
---llm_path seeklhy/codes-1b\
---sic_path ./sic_ckpts/sic_oncomx\
---table_num 25\
---column_num 106\
---dataset_path ./data/sft_oncomx_dev_text2sql.json\
---demonstration_set_path ./data/sft_oncomx_train_text2sql.json\
---num_of_demonstrations 1\
---max_tokens 8192\
+CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py \ 
+--llm_path seeklhy/codes-1b \
+--sic_path ./sic_ckpts/sic_oncomx \
+--table_num 25 \
+--column_num 106 \
+--dataset_path ./data/sft_oncomx_dev_text2sql.json \
+--demonstration_set_path ./data/sft_oncomx_train_text2sql.json \
+--num_of_demonstrations 1 \
+--max_tokens 8192 \
 --max_new_tokens 256
 #
 ## Oncomx's dev (3-shot)
