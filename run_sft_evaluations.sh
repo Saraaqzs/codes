d29cb3a7c80c927fbbbe2951b549e562bb853de6
Sara A. Safari <48481380+Saraaqzs@users.noreply.github.com>
2025-05-16 19:54:28 +0200

    adaptation to oncomx

--- run_sft_evaluations.sh
+++ run_sft_evaluations.sh
@@ -10,6 +10,7 @@ CUDA_VISIBLE_DEVICES=0 python -u text2sql_zero_shot.py \
 --max_tokens 4096 \
 --max_new_tokens 256
 
+
 # --------------- Spider dev --------------- #
 # SFT CodeS-1B on Spider's training set
 # CUDA_VISIBLE_DEVICES=0 python -u text2sql_zero_shot.py --llm_path seeklhy/codes-1b-spider --sic_path ./sic_ckpts/sic_spider --table_num 6 --column_num 10 --dataset_path ./data/sft_spider_dev_text2sql.json --max_tokens 4096 --max_new_tokens 256
