9abd0de18c1f8cf092c1179fc3cba10ad3822196
Sara A. Safari <48481380+Saraaqzs@users.noreply.github.com>
2025-05-20 16:35:02 +0200

    Update and rename run_few_shot_evaluation_sdss.sh to run_few_shot_evaluations_sdss.sh

--- run_few_shot_evaluations_sdss.sh
+++ run_few_shot_evaluations_sdss.sh
@@ -25,11 +25,11 @@ CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py --llm_path seeklhy/codes-7
 #
 # --------------- Few-shot CodeS-15B --------------- #
 # sdss' dev (0-shot)
-CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py --llm_path seeklhy/codes-15b --sic_path ./sic_ckpts/sic_cordis --table_num 6 --column_num 61 --dataset_path ./data/sft_sdss_dev_text2sql.json --pred_tag 15B_0S_sdss --demonstration_set_path ./data/sft_sdss_train_text2sql.json --num_of_demonstrations 0 --max_tokens 8192 --max_new_tokens 256
+CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py --llm_path seeklhy/codes-15b --sic_path ./sic_ckpts/sic_sdss --table_num 6 --column_num 61 --dataset_path ./data/sft_sdss_dev_text2sql.json --pred_tag 15B_0S_sdss --demonstration_set_path ./data/sft_sdss_train_text2sql.json --num_of_demonstrations 0 --max_tokens 8192 --max_new_tokens 256
 #
 ## sdss' dev (1-shot)
-CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py --llm_path seeklhy/codes-15b --sic_path ./sic_ckpts/sic_cordis --table_num 6 --column_num 61 --dataset_path ./data/sft_sdss_dev_text2sql.json --pred_tag 15B_1S_sdss --demonstration_set_path ./data/sft_sdss_train_text2sql.json --num_of_demonstrations 1 --max_tokens 8192 --max_new_tokens 256
+CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py --llm_path seeklhy/codes-15b --sic_path ./sic_ckpts/sic_sdss --table_num 6 --column_num 61 --dataset_path ./data/sft_sdss_dev_text2sql.json --pred_tag 15B_1S_sdss --demonstration_set_path ./data/sft_sdss_train_text2sql.json --num_of_demonstrations 1 --max_tokens 8192 --max_new_tokens 256
 #
 ## sdss' dev (3-shot)
-CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py --llm_path seeklhy/codes-15b --sic_path ./sic_ckpts/sic_cordis --table_num 6 --column_num 61 --dataset_path ./data/sft_sdss_dev_text2sql.json --pred_tag 15B_3S_sdss --demonstration_set_path ./data/sft_sdss_train_text2sql.json --num_of_demonstrations 3 --max_tokens 8192 --max_new_tokens 256
+CUDA_VISIBLE_DEVICES=0 python -u text2sql_few_shot.py --llm_path seeklhy/codes-15b --sic_path ./sic_ckpts/sic_sdss --table_num 6 --column_num 61 --dataset_path ./data/sft_sdss_dev_text2sql.json --pred_tag 15B_3S_sdss --demonstration_set_path ./data/sft_sdss_train_text2sql.json --num_of_demonstrations 3 --max_tokens 8192 --max_new_tokens 256
 #
